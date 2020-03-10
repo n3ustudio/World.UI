ac62c45d74e4849796fce5fe0d5f824bf155fbd5
Il Harper <afanyiyu@hotmail.com>
2020-03-10 11:40:27 +0800

    feat: F0ParameterData Changes

--- World.UI/Models/Parameter/F0ParameterData.cs
+++ World.UI/Models/Parameter/F0ParameterData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,16 @@ namespace World.UI.Models.Parameter
 {
     public sealed class F0ParameterData : ParameterData
     {
+
+        public F0ParameterData(
+            bool isNatural = false,
+            string name = "PIT",
+            List<double> parameterList = null)
+            :
+            base(isNatural, name, new ObservableCollection<double>(parameterList))
+        {
+
+        }
 
     }
 }
