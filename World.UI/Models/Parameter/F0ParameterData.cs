ee0f1986da80e383a5176b4c63b1533a8c67150e
Il Harper <afanyiyu@hotmail.com>
2020-03-10 11:31:34 +0800

    feat: Add F0ParameterData Class

---
+++ World.UI/Models/Parameter/F0ParameterData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Musiqual.Parameter;
+
+namespace World.UI.Models.Parameter
+{
+    public sealed class F0ParameterData : ParameterData
+    {
+
+    }
+}
