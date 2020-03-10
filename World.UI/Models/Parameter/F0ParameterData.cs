a917201736a3325a5723eb78078ca2ab9d4f4850
Il Harper <afanyiyu@hotmail.com>
2020-03-10 13:09:46 +0800

    feat: F0ParameterData Changes

--- World.UI/Models/Parameter/F0ParameterData.cs
+++ World.UI/Models/Parameter/F0ParameterData.cs
@@ -11,15 +11,51 @@ namespace World.UI.Models.Parameter
     public sealed class F0ParameterData : ParameterData
     {
 
-        public F0ParameterData(
-            bool isNatural = false,
-            string name = "PIT",
-            List<double> parameterList = null)
+        private F0ParameterData(
+            bool isNatural = true,
+            string name = "F0",
+            List<double> parameterList = null,
+            double tolerance = 0.00001)
             :
-            base(isNatural, name, new ObservableCollection<double>(parameterList))
+            base(isNatural, name, parameterList, tolerance)
         {
 
         }
 
+        public F0ParameterData CreateF0ParameterData(
+            List<double> parameterList = null,
+            double tolerance = 0.00001) =>
+            new F0ParameterData(true, "F0", parameterList, tolerance);
+
     }
+
+    public sealed class F0DeltaParameterData : ParameterData
+    {
+
+        private F0DeltaParameterData(
+            List<double> f0List,
+            bool isNatural = false,
+            string name = "PIT",
+            List<double> parameterList = null,
+            double tolerance = 0.00001)
+            :
+            base(isNatural, name, parameterList, tolerance)
+        {
+            f0OriginList = f0List;
+        }
+
+        public F0DeltaParameterData CreateF0DeltaParameterData(
+            List<double> parameterList = null,
+            double tolerance = 0.00001)
+        {
+            List<double> pList = new List<double>();
+            for (int i = 0; i < parameterList.Count; i++) pList.Add(0);
+
+            return new F0DeltaParameterData(f0OriginList, false, "PIT", pList, tolerance);
+        }
+
+        private List<double> f0OriginList { get; }
+
+    }
+
 }
