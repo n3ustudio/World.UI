ea803bece7a137676115dc56281d25a4cf4110a3
Il Harper <afanyiyu@hotmail.com>
2020-03-11 12:49:51 +0800

    feat: Add Posit

--- World.UI/Models/Parameter/F0ParameterData.cs
+++ World.UI/Models/Parameter/F0ParameterData.cs
@@ -12,6 +12,7 @@ namespace World.UI.Models.Parameter
     {
 
         private F0ParameterData(
+            long total,
             string name = "F0",
             List<double> parameterList = null,
             double tolerance = 0.00001,
@@ -19,7 +20,7 @@ namespace World.UI.Models.Parameter
             double viewMin = 0,
             double viewMax = 500)
             :
-            base(true, name, parameterList, tolerance, viewTotal, viewMin, viewMax)
+            base(total, true, name, parameterList, tolerance, viewTotal, viewMin, viewMax)
         {
 
         }
@@ -27,7 +28,7 @@ namespace World.UI.Models.Parameter
         public F0ParameterData CreateF0ParameterData(
             List<double> parameterList = null,
             double tolerance = 0.00001) =>
-            new F0ParameterData("F0", parameterList, tolerance);
+            new F0ParameterData(parameterList.Count, "F0", parameterList, tolerance);
 
     }
 
@@ -36,6 +37,7 @@ namespace World.UI.Models.Parameter
 
         private F0DeltaParameterData(
             List<double> f0List,
+            long total,
             string name = "PIT",
             List<double> parameterList = null,
             double tolerance = 0.00001,
@@ -43,7 +45,7 @@ namespace World.UI.Models.Parameter
             double viewMin = -250,
             double viewMax = 250)
             :
-            base(false, name, parameterList, tolerance, viewTotal, viewMin, viewMax)
+            base(total, false, name, parameterList, tolerance, viewTotal, viewMin, viewMax)
         {
             f0OriginList = f0List;
         }
@@ -55,7 +57,7 @@ namespace World.UI.Models.Parameter
             List<double> pList = new List<double>();
             for (int i = 0; i < parameterList.Count; i++) pList.Add(0);
 
-            return new F0DeltaParameterData(f0OriginList, "PIT", pList, tolerance);
+            return new F0DeltaParameterData(f0OriginList, f0OriginList.Count, "PIT", pList, tolerance);
         }
 
         private List<double> f0OriginList { get; }
