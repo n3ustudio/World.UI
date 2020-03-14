405798d4ce5f752c423f62a707c4f3863dd23ffd
Il Harper <afanyiyu@hotmail.com>
2020-03-14 22:06:28 +0800

    feat: Add F0 Data Export Methods

--- World.UI/Models/Parameter/F0ParameterData.cs
+++ World.UI/Models/Parameter/F0ParameterData.cs
@@ -29,6 +29,42 @@ namespace World.UI.Models.Parameter
             double tolerance = 0.00001) =>
             new F0ParameterData(parameterList.Count, 5000, "F0", parameterList, tolerance);
 
+        /// <summary>
+        /// Export the F0 data.
+        /// </summary>
+        /// <returns>The exported data.</returns>
+        public List<double> Export()
+        {
+            List<Musiqual.Models.Parameter> pList = new List<Musiqual.Models.Parameter>();
+            foreach (Musiqual.Models.Parameter parameter in ParameterList)
+            {
+                pList.Add(parameter);
+            }
+            pList.Sort((pA, pB) =>
+            {
+                if (pA.Position.Position > pB.Position.Position) return 1;
+                if (pA.Position.Position < pB.Position.Position) return -1;
+                return 0;
+            });
+            List<double> resultList = new List<double>();
+            int index = 0;
+            double prevData = 0;
+            foreach (Musiqual.Models.Parameter parameter in pList)
+            {
+                while (true)
+                {
+                    if (index >= parameter.Position.Position) break;
+                    resultList.Add(prevData);
+                    index++;
+                }
+                resultList.Add(parameter.Value.Position);
+                prevData = parameter.Value.Position;
+                index++;
+            }
+
+            return resultList;
+        }
+
     }
 
     public sealed class F0DeltaParameterData : ParameterData
@@ -58,6 +94,42 @@ namespace World.UI.Models.Parameter
         }
 
         private List<double> f0OriginList { get; }
+
+        /// <summary>
+        /// Export the F0 data.
+        /// </summary>
+        /// <returns>The exported data.</returns>
+        public List<double> Export()
+        {
+            List<Musiqual.Models.Parameter> pList = new List<Musiqual.Models.Parameter>();
+            foreach (Musiqual.Models.Parameter parameter in ParameterList)
+            {
+                pList.Add(parameter);
+            }
+            pList.Sort((pA, pB) =>
+            {
+                if (pA.Position.Position > pB.Position.Position) return 1;
+                if (pA.Position.Position < pB.Position.Position) return -1;
+                return 0;
+            });
+            List<double> resultList = new List<double>();
+            int index = 0;
+            double prevData = 0;
+            foreach (Musiqual.Models.Parameter parameter in pList)
+            {
+                while (true)
+                {
+                    if (index >= parameter.Position.Position) break;
+                    resultList.Add(f0OriginList[index] + prevData);
+                    index++;
+                }
+                resultList.Add(f0OriginList[index] + parameter.Value.Position);
+                prevData = parameter.Value.Position;
+                index++;
+            }
+
+            return resultList;
+        }
 
     }
 
